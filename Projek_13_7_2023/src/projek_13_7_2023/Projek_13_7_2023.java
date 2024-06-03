4883c8f4a44d3842d26cdc427ce4479beb042793
Seraph003 <114631454+Seraph003@users.noreply.github.com>
2024-06-04 01:07:25 +0700

    #1

---
+++ Projek_13_7_2023/src/projek_13_7_2023/Projek_13_7_2023.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projek_13_7_2023;
+
+/**
+ *
+ * @author erik
+ */
+public class Projek_13_7_2023 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
